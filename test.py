dcab3edd65ce555d95f7abf69610f14a3522f312
Tagashy <baboin.arthur@gmail.com>
2019-02-22 23:05:18 +0100

    commit 22/02/2019

---
+++ test.py
@@ -0,0 +1,34 @@
+from logging import basicConfig, DEBUG
+
+from Eranox.Server.Protocol.SSL import SocketServer
+from EranoxAuth import Authenticator, Role, DefaultEngine
+
+if __name__ == '__main__':
+    basicConfig(level=DEBUG)
+    auth = Authenticator(DefaultEngine("sqlite:///eranox.db"))
+    auth.create_user("ANONYMOUS", "", Role.ANONYMOUS)
+    auth.create_user("tagashy", "123456", Role.ROOT)
+    auth.create_user("admin", "admin", Role.ADMIN)
+    auth.create_user("test", "test", Role.USER)
+
+    ss = SocketServer(cert_path="Eranox/Server/data/certificate.crt",
+                      private_key_path="Eranox/Server/data/privatekey.key", authenticator=auth)
+    ss.start()
+    import time
+
+    time.sleep(200)
+
+    import socket
+    import ssl
+
+    hostname = '127.0.0.1'
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.load_verify_locations('Eranox/Server/data/certificate.crt')
+    context.check_hostname = False
+    with socket.create_connection((hostname, 8443)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname, ) as ssock:
+            print(ssock.version())
+            print(ssock.read())
+            print(ssock.read())
+            print(ssock.send('{"username":"toto","password":"123456"}'.encode("utf-8")))
+            print(ssock.read())
